8cc98745bb09b216e12f76e5c70224cce0315fae
flamedodger <12417792+flamedodger@users.noreply.github.com>
2025-09-17 00:58:40 +0100

    Update a02yyuw_triggered.h

--- components/a02yyuw_triggered/a02yyuw_triggered.h
+++ components/a02yyuw_triggered/a02yyuw_triggered.h
@@ -1,1 +1,20 @@
+#pragma once
+#include "esphome.h"
+
+namespace esphome {
+namespace a02yyuw_triggered {
+
+class A02YYUWTriggeredSensor : public PollingComponent, public sensor::Sensor {
+ public:
+  output::BinaryOutput *trigger_;
+  uart::UARTComponent *uart_;
+
+  A02YYUWTriggeredSensor(output::BinaryOutput *trigger, uart::UARTComponent *uart)
+      : PollingComponent(500), trigger_(trigger), uart_(uart) {}
+
+  void update() override;
+};
+
+}  // namespace a02yyuw_triggered
+}  // namespace esphome
 
